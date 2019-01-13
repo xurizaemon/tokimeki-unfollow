6a7c1eea53625928d5258f8e81ee89c8881d6869
Glitch (hello-express) <none>
2019-01-13 08:47:53 +0000

    🐣🌅 Checkpoint ./server.js:2748687/1073 ./views/layout.pug:2748687/1024 ./views/index.pug:2748687/1932

--- server.js
+++ server.js
@@ -2,12 +2,29 @@
 // where your node app starts
 
 // init project
+// install modules 'npm install'
+// also, install pug for templating
 const express = require('express');
 const passport = require('passport');
 let {Strategy} = require('passport-twitter');
 const session = require('cookie-session');
 const twitter = require('twit');
+
+// Temp holder vars we need to store in session
+let token, secret;
+
 const app = express();
+
+// setup passport and session manager
+app.use(session({
+  name: 'session',
+  secret: 'keyboard cat',
+  maxAge: 24 * 60 * 60 * 1000
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.set('view engine', 'pug');
+
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -17,7 +34,12 @@ passport.use(new Strategy({
   consumerKey: process.env.KEY,
   consumerSecret: process.env.SECRET,
   callbackURL: '/oauth/callback/twitter'
-}, (token, secret, profile, done) => {
+}, (authToken, authSecret, profile, done) => {
+  token = authToken;
+  secret = authSecret;
+  console.log('token', token);
+  console.log('secret', secret);
+  console.log('profile', profile);
   return done(null, profile);
 }));
 
@@ -31,17 +53,24 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index');
 });
 
-// setup login route to link to 
+app.get('/review', (req, res) => {
+  res.render('review', {
+    following: [],
+  });
+});
+
+// setup login route to link to with login link on website
 app.get('/login/twitter',
   passport.authenticate('twitter'));
 
-app.get('/oauth/callback/twitter.com',
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
+// callback url, must add this to your app on twitters developer portal
+app.get('/oauth/callback/twitter',
+  passport.authenticate('twitter', { failureRedirect: '/' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('/review');
   });
 
 // listen for requests :)
