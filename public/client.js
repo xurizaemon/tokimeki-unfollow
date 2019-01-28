8356112d0ea0745d5398b80c0ba4ce33cfaf1e64
Glitch (tokimeki-unfollow) <none>
2019-01-28 06:44:08 +0000

    😱🎩 Checkpoint ./public/client.js:2748687/1428 ./views/review.html:2748687/57

--- public/client.js
+++ public/client.js
@@ -65,7 +65,7 @@ function render(res) {
       showBio: false,
       prefs: {
         showBio: false,
-        order: 'newest',
+        order: 'oldest',
         saveProgressAsList: true
       },
       unfollowed: [],
@@ -147,6 +147,14 @@ function render(res) {
     created: function() {
       this.loadProgressQuick();
       this.loadProgressList();
+      switch (this.prefs.order) {
+        case 'random':
+          this.friends = shuffle(this.friends);
+          break;
+        case 'oldest':
+          this.friends = this.friends.reverse();
+          break;
+      }
     },
     watch: {
       selFriendId: {
@@ -178,3 +186,30 @@ function render(res) {
     }
   });
 }
+
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+function shuffle(array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
