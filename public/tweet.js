749da9b3ce05e85d316fece9101c991efd19689a
Glitch (tokimeki-unfollow) <none>
2019-01-29 09:48:14 +0000

    ⛰🎖 Checkpoint ./public/tweet.js:2748687/1360 ./public/style.css:2748687/116

--- public/tweet.js
+++ public/tweet.js
@@ -15,12 +15,26 @@ let tweetComponent = Vue.component('tweet', {
         &#x1f501;&#xFE0E; Retweeted
       </div>
       <div class="backup-tweet">
-        <img v-bind:src="tweet.user.profile_image_url_https" class="backup-tweet-avatar">
-        <a href="https://twitter.com/{{ tweet.user.screen_name }}">
-          <strong>{{ tweet.user.name }}</strong>
-          <span class="gray">
-            @{{ tweet.user.screen_name }} ·
+        <a :href="'https://twitter.com/' + tweet.user.screen_name"
+            target="_blank" rel="noopener noreferrer"
+            class="nodec">
+          <img v-bind:src="tweet.user.profile_image_url_https" class="backup-tweet-avatar">
+        </a>
+        <span>
+          <a :href="'https://twitter.com/' + tweet.user.screen_name"
+            target="_blank" rel="noopener noreferrer"
+            class="nodec">
+            <strong class="black">{{ tweet.user.name }}</strong>
+            <span class="gray">
+              @{{ tweet.user.screen_name }}
+            </span>
+          </a>
+          <span class="gray">·
+          <a :href="'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str"
+            target="_blank" rel="noopener noreferrer"
+            class="gray nodec">
             <vue2-timeago :datetime="new Date(tweet.created_at)"></vue2-timeago>
+          </a>
           </span>
         </span>
         <br>
