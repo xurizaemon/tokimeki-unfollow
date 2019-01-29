9416f24c4a581903665a4448caaaf0bd23e8944f
Glitch (tokimeki-unfollow) <none>
2019-01-29 09:26:14 +0000

    ❄️🐙 Checkpoint ./public/tweet.js:2748687/445 ./public/style.css:2748687/202

--- public/tweet.js
+++ public/tweet.js
@@ -10,21 +10,26 @@ let tweetComponent = Vue.component('tweet', {
     }
   },
   template:`
-    <li class="backup-tweet">
-      <div v-if="tweet.retweeted_by">{{ tweet.retweeted_by }} Retweeted</div>
-      <img v-bind:src="tweet.user.profile_image_url_https" class="backup-tweet-avatar">
-      <span>
-        <strong>{{ tweet.user.name }}</strong>
-        <span class="gray">
-          @{{ tweet.user.screen_name }} ·
-          <vue2-timeago :datetime="new Date(tweet.created_at)"></vue2-timeago>
+    <li>
+      <div v-if="tweet.retweeted_by" class="retweet gray w100">
+        &#x1f501;&#xFE0E; Retweeted
+      </div>
+      <div class="backup-tweet">
+        <img v-bind:src="tweet.user.profile_image_url_https" class="backup-tweet-avatar">
+        <span>
+          <strong>{{ tweet.user.name }}</strong>
+          <span class="gray">
+            @{{ tweet.user.screen_name }} ·
+            <vue2-timeago :datetime="new Date(tweet.created_at)"></vue2-timeago>
+          </span>
         </span>
-      </span>
-      <p v-html="formatTweetText(tweet.text)"></p>
-      <img
-        v-if='tweet.entities && tweet.entities.media && tweet.entities.media[0]'
-        v-bind:src='tweet.entities.media[0].media_url_https'
-        class="w100">
+        <br>
+        <span v-html="formatTweetText(tweet.text)"></span>
+        <img
+          v-if='tweet.entities && tweet.entities.media && tweet.entities.media[0]'
+          v-bind:src='tweet.entities.media[0].media_url_https'
+          class="w100">
+      </div>
     </li>
   `
 });
